8d45ece2f7aa2ebd9e40cf22f75692506619108a
Muhammad Sajid <rexisajid5@gmail.com>
2025-05-08 17:34:11 +0000

    make bacground acording to themes, bacground will be animated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -103,7 +103,7 @@ export default function RootLayout({
 
                 <div className="flex flex-col flex-1 overflow-hidden">
                   <AppHeader />
-                  <main className="flex-1 p-4 sm:p-6 overflow-y-auto bg-background"> 
+                  <main className="flex-1 p-4 sm:p-6 overflow-y-auto animate-background-pan"> 
                     {children}
                   </main>
                 </div>
